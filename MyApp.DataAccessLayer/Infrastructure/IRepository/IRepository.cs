4965d234f0721e963258c161c464aa88a01d5b20
Bilal Shabbir <bilalshabbirkind@gmail.com>
2022-06-18 00:56:36 +0500

    Repository Pattern Implement

---
+++ MyApp.DataAccessLayer/Infrastructure/IRepository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.DataAccessLayer.Infrastructure.IRepository
+{
+    public class IRepository<T> where T:class
+    {
+        IEnumerable<T> GetAll();
+    }
+}
